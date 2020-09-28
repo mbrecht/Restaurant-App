791303d721fc95cea7c3299c42ff5b6d14215975
Michael Brecht <brecht.michael.b@gmail.com>
2020-09-28 15:01:00 -0600

    (init)

---
+++ test/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from '../src/components/App';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+describe('Initial testing', () => {
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<App />))
+
+  test('Should contain an app component', () => {
+    expect(wrapper.find('#app').exists()).toBe(true);
+  })
+
+  test('Should contain an H1 with "Hello, World"', () => {
+    expect(wrapper.find('h1').text()).toBe('Hello, World')
+  })
+})
