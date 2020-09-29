34fba11e1d905676f42bed98e1c741723f80a6c3
Michael Brecht <brecht.michael.b@gmail.com>
2020-09-29 06:19:36 -0600

    implements pagination

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,64 +1,84 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const keys = ['name', 'city', 'state', 'telephone', 'genre'];
-
-const TableContainer = styled.table``
-
-const TableHead = styled.thead``
-
-const TableBody = styled.tbody``
-
-const TR = styled.tr``
-
-const TH = styled.th``
-
-const TD = styled.td``
-
-const filterByKeys = (restaurant) => (
-  Object
-    // keys are inherited through closure
-    .keys(restaurant)
-    .filter(key => keys.includes(key))
-    .reduce((obj, key) => {
-      obj[key] = restaurant[key];
-      return obj;
-    }, {})
-)
-
-const sortByName = (a, b) => {
-  if(a.name < b.name) return -1;
-  if(b.name < a.name) return 1;
-  return 0;
-}
-
-const renderRow = (restaurant, i) => (
-  <TR key={i}>
-    {
-      keys.map((key, j) => <TD key={j}>{restaurant[key]}</TD>)
-    }
-  </TR>
-)
+import React, { useState, useEffect } from 'react';
+import {
+  TableContainer,
+  StyledTable,
+  TableHead,
+  TableBody,
+  TR,
+  TH,
+  TD,
+  FirstButton,
+  PrevButton,
+  NextButton,
+  LastButton
+} from './TableComponents.jsx';
 
 const Table = ({ restaurants }) => {
+  const keys = ['name', 'city', 'state', 'telephone', 'genre'];
+
+  const filterByKeys = (restaurant) => (
+    Object
+      // keys are inherited through closure
+      .keys(restaurant)
+      .filter(key => keys.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = restaurant[key];
+        return obj;
+      }, {})
+  )
+
+  const sortByName = (a, b) => {
+    if(a.name < b.name) return -1;
+    if(b.name < a.name) return 1;
+    return 0;
+  }
+
+  const renderRow = (restaurant, i) => (
+    <TR key={i}>
+      {
+        keys.map((key, j) => <TD key={j}>{restaurant[key]}</TD>)
+      }
+    </TR>
+  )
+
+  const [page, setPage] = useState(0);
+  const [count, setCount] = useState(10);
+
+  const lastPage = Math.floor(restaurants.length / 10);
+
+  const handleClick = (e, targetPage) => {
+    e.preventDefault();
+
+    targetPage < 0 ? targetPage = 0 : null;
+    targetPage > lastPage ? targetPage = lastPage : null;
+
+    setPage(targetPage);
+  }
 
   return (
     <TableContainer className="table">
-      <TableHead className="table-head">
-        <TR>
+      <StyledTable>
+        <TableHead className="table-head">
+          <TR>
+            {
+              keys.map((keyName, i) => <TH key={i}>{keyName}</TH>)
+            }
+          </TR>
+        </TableHead>
+        <TableBody className="table-body">
           {
-            keys.map((keyName, i) => <TH key={i}>{keyName}</TH>)
+            restaurants
+              .map(filterByKeys)
+              .sort(sortByName)
+              .slice(page * count, page * count + count)
+              .map(renderRow)
           }
-        </TR>
-      </TableHead>
-      <TableBody className="table-body">
-        {
-          restaurants
-            .map(filterByKeys)
-            .sort(sortByName)
-            .map(renderRow)
-        }
-      </TableBody>
+        </TableBody>
+      </StyledTable>
+      <FirstButton onClick={e => handleClick(e, 0)} />
+      <PrevButton onClick={e => handleClick(e, page - 1)} />
+      <NextButton onClick={e => handleClick(e, page + 1)} />
+      <LastButton onClick={e => handleClick(e, lastPage)} />
     </TableContainer>
   )
 }
