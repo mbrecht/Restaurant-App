78d1268aa8c73c91b22bed70876a28905f77af57
Michael Brecht <brecht.michael.b@gmail.com>
2020-09-30 10:03:54 -0600

    improves compression

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(compression());
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
