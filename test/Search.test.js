22bdadf19bdd325b1cf879a74b8f9dceb7f8b296
Michael Brecht <brecht.michael.b@gmail.com>
2020-09-29 18:21:01 -0600

    ensures search component renders correctly

--- test/Search.test.js
+++ test/Search.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Search from '../src/components/Search';
+import restaurants from './restaurants.json';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-describe('Initial testing', () => {
+describe('Ensure all elements are rendering', () => {
   let wrapper;
 
-  beforeEach(() => wrapper = shallow(<Search restaurants={[]}/>))
+  beforeEach(() => wrapper = shallow(<Search restaurants={restaurants}/>))
 
-  test('Should contain an search component', () => {
-    expect(wrapper.find('.search').exists()).toBe(true);
+  test('Should contain an input element', () => {
+    expect(wrapper.find('.search-input').exists()).toBe(true);
+  })
+
+  test('Should contain a search button', () => {
+    expect(wrapper.find('.search-button').exists()).toBe(true);
+  })
+
+  test('Button should indicate what it does', () => {
+    expect(wrapper.find('.search-button').text()).toBe('Search');
   })
 })
